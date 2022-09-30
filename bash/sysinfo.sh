864d69c9ab0b102a87d65fe3b42122d6758b38af
AshuTamang <comp2101-02@zonzorp.net>
2022-09-30 11:12:11 -0400

    sending my files

---
+++ bash/sysinfo.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#command to go in the bash directory 
+hostname -f
+#helps differntiate device within the local network
+hostnamectl
+# changes the system hostname and related setting 
+hostname - |
+#helps differntiate device within the local network
+df -h | grep -w/
